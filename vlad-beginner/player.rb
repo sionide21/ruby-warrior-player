dd4b1c05d09707c419dfd07ade11e73584deb6e5
Ben Olive <sionide21@gmail.com>
2013-08-01 08:13:06 -0400

    Level 4

--- vlad-beginner/player.rb
+++ vlad-beginner/player.rb
@@ -1,13 +1,58 @@
 class Player
+  attr_reader :warrior, :old_health
+
+  def initialize
+    @old_health = 20
+  end
+
+  def under_attack?
+    warrior.health < old_health
+  end
+
   def play_turn(warrior)
-    if warrior.feel.empty?
-      if warrior.health < 19
-        warrior.rest!
-      else
-        warrior.walk!
-      end
+    @warrior = warrior
+    case
+    when under_attack?
+      vlad = DangerVald
     else
-      warrior.attack!
+      vlad = Vlad
     end
+    vlad.new(warrior).act!
+    @old_health = warrior.health
+  end
+end
+
+class Vlad
+  attr_reader :warrior
+  def initialize(warrior)
+    @warrior = warrior
+  end
+
+  def hurt?
+    warrior.health < 19
+  end
+
+  def should_fight?
+    warrior.feel.enemy?
+  end
+  def should_heal?
+    hurt?
+  end
+
+  def act!
+    case
+    when should_fight?
+      warrior.attack!
+    when should_heal?
+      warrior.rest!
+    else
+      warrior.walk!
+    end
+  end
+end
+
+class DangerVald < Vlad
+  def should_heal?
+    false
   end
 end
